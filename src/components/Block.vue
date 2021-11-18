3bdee4b3443cd8b0a66c31686e2b276f65e1c484
brandonidun <biduntawiah@gmail.com>
2021-11-18 16:35:22 +0000

    First commit

---
+++ src/components/Block.vue
@@ -0,0 +1,17 @@
+
+<template><Entries /></template>
+
+<script>
+import Entry from "./Entries";
+
+export default {
+  component: { Entry },
+  props: [],
+  data() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Resume.vue
@@ -0,0 +1,25 @@
+<template>
+  <Block />
+  <PersonalDetails />
+</template>
+
+<script>
+import Block from "./Block.vue";
+import PersonalDetails from "./PersonalDetails";
+
+export default {
+  components: {
+    Block,
+    PersonalDetails,
+  },
+
+  data() {},
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style>
+</style> 
